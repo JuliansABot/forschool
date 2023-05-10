5a20072670cb7d064a258e667c2432ddf2737a3d
JuliansABot <122099740+JuliansABot@users.noreply.github.com>
2023-05-10 08:43:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("Hello we are DJGames and we our a organization in school");
+alert("Hello we are DJGames and we our a organization in school as well as developers that like to code unblocked wesites");
